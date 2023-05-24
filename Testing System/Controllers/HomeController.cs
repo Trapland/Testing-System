455657426bd64c5dc3fab1d0306fc75e5941de19
Trapland <89914789+Trapland@users.noreply.github.com>
2023-05-24 22:36:04 +0200

    login, registration improves

--- Testing System/Controllers/HomeController.cs
+++ Testing System/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Testing_System.Controllers
             return View();
         }
 
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
